5bc98d46d0c4cd5583418a25aa616c9a397028d1
Mudskippy <73501440+endertuber123@users.noreply.github.com>
2023-09-17 15:53:06 -0500

    Proper changes made to C#Bridge branch
    Adds the C# part of the C#Bridge and removes the Node.js code, as that code is for the main branch.

---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using System.Net;
+
+using Rug.Osc;
+
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+
+public class OSCSocket : WebSocketBehavior  //Not the best name, but it gets the job done.
+{
+    const int OUT_PORT = 9000;
+    private OscSender toVRChat;
+
+
+    public OSCSocket()
+    {
+        toVRChat = new OscSender(IPAddress.Parse("127.0.0.1"),OUT_PORT);
+        toVRChat.Connect();
+    }
+
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        //This sections of code is for getting messages sent from the web page to VRChat.
+        base.OnMessage(e);
+        switch (e.Data.ToString())
+        {
+            case "Automatic":
+                toVRChat.Send(new OscMessage("/avatar/parameters/isManual", 0));
+                break;
+            case "Manual":
+                toVRChat.Send(new OscMessage("/avatar/parameters/isManual", 1));
+                break;
+            case "Playlist":
+                toVRChat.Send(new OscMessage("/avatar/parameters/isSingle", 0));
+                break;
+            case "Single":
+                toVRChat.Send(new OscMessage("/avatar/parameters/isSingle", 1));
+                break;
+            default:
+                if (e.Data.ToString().StartsWith("Chat:"))
+                {
+                    String tempChat = e.Data.ToString().Replace("Chat:", "");
+                    toVRChat.Send(new OscMessage("/chatbox/input", tempChat, 1));
+                }
+                break;
+        }
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        WebSocketServer serverSocket = new("ws://localhost:2588");
+
+        serverSocket.AddWebSocketService<OSCSocket>("/Bridge");
+
+        serverSocket.Start();
+        Console.WriteLine("WebSocket server started. Web page should be able to connect now.");
+
+        //OscReveiver is here to get signals from VRChat.
+        const int IN_PORT = 9001;
+        OscReceiver fromVRChat = new(IN_PORT);
+        try
+        {
+            fromVRChat.Connect();
+            Console.WriteLine("OSC server started. Will begin listening for VRChat\'s signals.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("There was a problem starting the OSC server. Most likely caused by the program not being allowed through the Firewall in order receive from VRChat\'s port. Sometimes, a restart is also required to apply changes to the Firewall.");
+            Console.WriteLine(e.ToString());
+
+            serverSocket.Stop();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            return;
+        }
+
+        bool nextGate = false;
+        bool pauseGate = false;
+        bool backGate = false;
+        while (fromVRChat.State != OscSocketState.Closed)
+        {
+            //No need to recieve anything if it's not connected.
+            if (fromVRChat.State == OscSocketState.Connected)
+            {
+                //Obviously, OSC needs to be received.
+                OscPacket packetVRC = fromVRChat.Receive();
+
+                //Packet format is useless. An OscMessage object is necessary to check the address, which is important.
+                OscMessage messageVRC = (OscMessage)packetVRC;
+
+                //This is what does most of the work. The address and arguments are used to determine what WebSocket message to send to the web page.
+                //The purpose of this switch is to get information from VRChat to the web page.
+                switch (messageVRC.Address)
+                {
+                    //The _Angle addresses should only send when the lever is pulled all the way down.
+                    //To prevent spamming, the lever has to go back up again before it can be pulled again.
+                    case "/avatar/parameters/Next_Angle":
+                        float nextAngle = Convert.ToSingle(messageVRC[0]);
+                        if (nextGate == true && nextAngle <= 0.01) nextGate = false;
+                        else if (nextGate == false && nextAngle >= 0.49)
+                        {
+                            nextGate = true;
+                            serverSocket.WebSocketServices["/Bridge"].Sessions.Broadcast("Next");
+                            Console.WriteLine("Next sent from VRChat!");
+                        }
+                        break;
+                    case "/avatar/parameters/Pause_Angle":
+                        float pauseAngle = Convert.ToSingle(messageVRC[0]);
+                        if (pauseGate == true && pauseAngle <= 0.01) pauseGate = false;
+                        else if (pauseGate == false && pauseAngle >= 0.49)
+                        {
+                            pauseGate = true;
+                            serverSocket.WebSocketServices["/Bridge"].Sessions.Broadcast("Pause");
+                            Console.WriteLine("Pause sent from VRChat!");
+                        }
+                        break;
+                    case "/avatar/parameters/Back_Angle":
+                        float backAngle = Convert.ToSingle(messageVRC[0]);
+                        if (backGate == true && backAngle <= 0.01) backGate = false;
+                        else if (backGate == false && backAngle >= 0.49)
+                        {
+                            backGate = true;
+                            serverSocket.WebSocketServices["/Bridge"].Sessions.Broadcast("Back");
+                            Console.WriteLine("Back sent from VRChat!");
+                        }
+                        break;
+                    //This section is for the VRChat Remote Control, or the VRCRC.
+                    case "/VRCRC/NextTrack":
+                        serverSocket.WebSocketServices["/Bridge"].Sessions.Broadcast("Next");
+                        Console.WriteLine("Next sent from VRCRC!");
+                        break;
+                    case "/VRCRC/PlayPauseTrack":
+                        serverSocket.WebSocketServices["/Bridge"].Sessions.Broadcast("Pause");
+                        Console.WriteLine("Pause sent from VRCRC!");
+                        break;
+                    case "/VRCRC/PreviousTrack":
+                        serverSocket.WebSocketServices["/Bridge"].Sessions.Broadcast("Back");
+                        Console.WriteLine("Back sent from VRCRC!");
+                        break;
+                    case "/VRCRC/RestartTrack":
+                        serverSocket.WebSocketServices["/Bridge"].Sessions.Broadcast("Restart");
+                        Console.WriteLine("Restart sent from VRCRC!");
+                        break;
+                    case "/VRCRC/YouTubeURL":
+                        serverSocket.WebSocketServices["/Bridge"].Sessions.Broadcast("URL:" + messageVRC[0].ToString());
+                        Console.WriteLine("URL sent from VRCRC!");
+                        break;
+                    //This section is a bit messy due to needing to check for prefixes and extracting information from the address.
+                    default:
+                        if (messageVRC.Address.StartsWith("/avatar/parameters/music_disc") || messageVRC.Address.StartsWith("/avatar/parameters/album_") && (bool)messageVRC[0])
+                        {
+                            string tempAddress = messageVRC.Address.ToString().Replace("/avatar/parameters/", "");
+                            Console.WriteLine("Disc or Album sent from VRChat!");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,31 +2,27 @@
 
 This program is intended to be used with a specific VRChat avatar on PC. This program not only plays music for other users in VRChat, but if a playlist is loaded users can actually navigate through the playlist. The users can also pause/resume the current track using the middle lever of the avatar.
 
-This has only been tested on Windows 11 and Google Chrome. Results may vary on different setups.
+This has been tested on Windows 10 and 11. Also been tested with Google Chrome and Microsoft Edge. Anything else may have varying results.
 
 ## Installation Instructions
 1: You'll need something that forwards audio output to microphone input. VB-Audio does this pretty well, but theoretically anything that does this should work. https://vb-audio.com/Cable/VirtualCables.htm
 
-2: You'll need Node.js to run the NodeBridge.js script. https://nodejs.org/en/download
+2: The fact you're reading this implies you have a browser that supports JavaScript, so you're good on that.
 
-3: The fact you're reading this implies you have a Browser that supports JavaScript, so you're good on that. Google Chrome will work. I really shouldn't have to tell you how to get Google Chrome.
-
-4: Probably goes without saying, but make sure to extract the files if you download this as a zip folder.
-
-5: While in the folder containing the code, you may need to run 'npm install' to get the dependencies. The only direct dependency the script has is the "osc" module, but that may have it's own dependencies.
+3: Probably goes without saying, but make sure to extract the files if you download this as a zip folder.
 
 ## Setup Instructions
-1: You may either attempt to use the scuffed .sh file, or just manually run NodeBridge.js using "node NodeBridge.js" in a command prompt.
+1: Run the executable file (If you got from Releases) or compile the source code yourself (If you just cloned the repository). Make sure you allow Firewall access in the popup if it shows up. This is needed for Open Sound Control to work.
 
-2: Open the HTML file. This needs to be done AFTER the NodeBridge.js has been opened. Socket connections will screw up if it's done the other way. If you ran the scuffed batch file, both steps 1 and 2 should be done automatically.
+2: Open the HTML file. The webpage should connect to the bridge. If you opened the HTML file before the executable, you may need to click the re-connect button.
 
 3: Open VRChat, if you haven't already.
 
 4: Equip the "Jukebox OSC (Minecraft)" avatar (Name may be subject to change). This can be done anytime, if you feel like it. If you can't find the avatar, here's a link: https://vrchat.com/home/avatar/avtr_bb03466d-31c8-456e-933a-387f5b0515c2
 
-5: Change your VRChat settings so the microphone input is set to VB-Audio's virtual mic. Use the volume mixer to set Google Chrome's audio output to VB-Audio's virtual speaker. Audio played from your browser should now be outputted to VRChat!
+5: Change your VRChat settings so the microphone input is set to the virtual microphone (VB-Audio's, if you used that). Use the volume mixer to set your browser's audio output to the associated virtual speaker. Audio played from your browser should now be outputted to VRChat!
 
-6: ENABLE OSC IN VRCHAT! This can be done in the action (wheel) menu by going to Settings>OSC>Enable. If it's already enabled, you can skip this step.
+6: ENABLE OSC IN VRCHAT IF IT'S NOT ALREADY! This can be done in the action (wheel) menu by going to Settings>OSC>Enable. If it's already enabled, you can skip this step.
 
 7: Make sure your Avatar Interactions are set to everyone so they can pull the levers.
 
@@ -36,11 +32,14 @@ This has only been tested on Windows 11 and Google Chrome. Results may vary on d
 In automatic mode, the special avatar can have several effects on the webpage. The Next and Back levers of the avatar only affect playlists. As expected, they will play the Next or Previous video in the playlist, respectively. The Play/Pause lever will pause a playing video and play a paused video. The top of the avatar has a slot for discs. It's compatible with discs held by the "0valler (Minecraft)" avatar. https://vrchat.com/home/avatar/avtr_15149189-454c-45cd-b443-29e3e581b947 . When a disc is inserted, it will play a video corresponding to the disc (normally, the music of that disc).
 
 ## Operating the webpage
-The HTML is effectively the GUI of the program. The input text box expects for Youtube URLs to be inputted. This will change what video is being played. It accepts normal links, shortened links, and even playlist links. However, it won't work with YouTube Music links. Due to embed limitations, certain videos aren't compatible. Blame YouTube for this obstacle.
+The HTML is effectively the GUI of the program. The input text box expects for Youtube URLs to be inputted. This will change what video is being played. It accepts normal links, shortened links, and even playlist links. However, it won't work with YouTube Music links. Due to embed limitations, certain videos aren't compatible.
 
-The webpage has 2 modes: Manual and Automatic. Manual is for when you don't want the Levers/Discs to have an effect on the video. This is intended for if you want to manually play videos on the youtube site that don't work on the webpage. Turning off Avatar Interactions basically does the same thing. Automatic is for when you want Levers/Discs to have an effect.
+The webpage has 2 modes: Manual and Automatic. Manual is for when you don't want the Levers/Discs to have an effect on the video. This is intended for if you want to manually play videos on the youtube site that don't work on the webpage. Turning off Avatar Interactions basically does the same thing, but Manual mode also hides the levers. Automatic is for when you want Levers/Discs to have an effect.
 
 Remember: Even in Automatic mode, you can still interact with the embed video itself for direct control. Really helpful if you want to get to a particular video in a playlist.
 
 ## What's with the chatbox?
-The program utilizes VRChat's chatbox to get information across about the video. This includes the video's title and how far through the video is through playing. This information updates every 2 seconds. This also lets players know when the jukebox is in Manual mode. 
+The program utilizes VRChat's chatbox to get information across about the video. This includes the video's title and how far through the video is through playing. This information updates every 2 seconds. This also lets players know when the jukebox is in Manual mode.
+
+## Why is the chatbox sometimes not updating?
+This is a consequence of VRCHat limiting how often you can update the chatbox. If messages are sent too frequently, it'll stop accepting for a bit.
